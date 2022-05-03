6c6c980c43c0fd1fdc2673d5a1f40ecd93fdfb98
braneIT <brane.panic93@gmail.com>
2022-05-03 15:33:40 +0200

    Prvi dan kursa

---
+++ day 1/script.js
@@ -0,0 +1,51 @@
+//1 comments can make code readable 
+//2 Welcome to 30DaysOfJavaScript
+//3 
+/*comments can make code readable, 
+        easy to reuse and informative*/
+
+//4
+let string = "String";
+let boolean = true;
+let neodredjena;
+let nula = null;
+
+//5
+console.log(typeof "string");
+console.log(typeof boolean);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+//6
+let x;
+
+//7
+let y = 5;
+
+//8
+// let firstName = "Branislav";
+// let lastName = "Panic"
+// let maritalStatus = false;
+// let age = 22;
+// let country = "Serbia";
+
+// console.log(firstName);
+// console.log(lastName);        
+// console.log(maritalStatus);
+// console.log(age);
+// console.log(country);
+
+//9
+let firstName = "Branislav";
+let lastName = "Panic";
+let maritalStatus = false;
+let age = 22;
+let country = "Serbia";
+console.log(`i am ${firstName} ${lastName}, my marital status is ${maritalStatus}, and i am ${age} and from ${country}`);
+
+//10
+let myage = "I am 25 years old.";
+let yourage = "You are 30 years old.";
+console.log(myage);
+console.log(yourage);
