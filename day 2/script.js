e5c75449e3950f89863346da9fe7a7efe795896d
braneIT <brane.panic93@gmail.com>
2022-05-04 13:35:07 +0200

    grana dana 2

--- day 2/script.js
+++ day 2/script.js
@@ -106,11 +106,5 @@ let love = 'Love is the best thing in this world. Some found their love and some
 console.log(love.match(/\love\b/gi));
 //3.2
 console.log(x.match(/because/gi));
-//3.3
-const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-sentence.replace("@", " ");
-sentence.replace("%", " ");
-sentence.replace("#", " ");
-console.log(sentence);
-console.log(sentence.replace('@/g', 'dik'))
+
 
